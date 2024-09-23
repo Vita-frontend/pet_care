556f9b2563955d68f6e2d2585628b521d04b30ff
Vita-frontend <maletska89@gmail.com>
2024-09-23 13:56:22 +0200

    js error

---
+++ js/index.js
@@ -0,0 +1,20 @@
+(function () {
+  const iconBurger = document.querySelector(".header__nav-burger");
+  const navigationsBody = document.querySelector(".header__nav-wrapper");
+
+  iconBurger.addEventListener("click", function (e) {
+    document.body.classList.toggle("lock");
+    iconBurger.classList.toggle("active");
+    navigationsBody.classList.toggle("active");
+  });
+  const navLinks = document.querySelectorAll(".header__menu-link");
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", (_) => {
+      if (iconBurger.classList.contains("active")) {
+        document.body.classList.remove("lock");
+        iconBurger.classList.remove("active");
+        navigationsBody.classList.remove("active");
+      }
+    });
+  });
+})();
